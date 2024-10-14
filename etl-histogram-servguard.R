c0bf0c5ed4ce460c56d42143eddc532a57cdd56b
CaioAcayabaFurtado <caio.furtado@sptech.school>
2024-10-14 19:44:59 -0300

    comentando e aprimorando o código de ETL

--- etl-histogram-servguard.R
+++ etl-histogram-servguard.R
@@ -4,7 +4,7 @@
 
 #Captura de dados para o histograma:
 #-------------------------------------------------------------------------------
-# escolhaBaseDeDados <- "SQL"
+#escolhaBaseDeDados <- "SQL"
 #escolhaBaseDeDados <- "CSV"
 escolhaBaseDeDados <- "simulacao"
 
@@ -14,9 +14,9 @@ if (escolhaBaseDeDados == "SQL") {
   install.packages("RMySQL") 
   
   conexao <- dbConnect(RMySQL::MySQL(),
-                       dbname = "ServGuard",
+                       dbname = "ServGuard",#Nome do banco de dados
                        host = "127.0.0.1",#IP público da instância
-                       port = 3306,
+                       port = 3306, 
                        user = "root",
                        password = "urubu100")
   
@@ -29,13 +29,14 @@ if (escolhaBaseDeDados == "SQL") {
 } else if (escolhaBaseDeDados == "CSV"){
   #colocar o codigo de captura dos dados por CSV aqui
   captura <- read.csv("C:/Users/cacay/Downloads/dados-pc.csv", sep=",")
-  captura
   #Remove o "%" caso exista na coluna:
   usoCPU <- as.numeric(gsub("%", "", captura$CPU))  
 } else {
   #Teste com valores simulados:
   n <- 100
-  usoCPU <- runif(n, min = 0, max = 100)
+  set.seed(22)
+  usoCPU <- sample(0:100, n, replace = TRUE)
+  #usoCPU <- runif(n, min = 0, max = 100)
   
 }
 #-------------------------------------------------------------------------------
@@ -53,15 +54,19 @@ histograma <- hist(usoCPU, #Dados utilizados
                    freq=TRUE, 
                    right = FALSE, #Extende o y a maior frequencia histograma
                    xlim = c(0,100))#Fixa o valor de 0 a 100 na exibição 
+                   ylim = c(0, max(histograma$counts)+2)#Fixa o valor de 0 ao max do histograma 
+
+#Colocando o valor de frequencia em cima das colunas.
+text(x = histograma$mids, #Posição x
+     y = histograma$counts, #Posição y
+     labels = histograma$counts, #valor do texto
+     pos = 3, #Ponto aonde o texto fica, no caso 3 significa acima da coluna 
+     cex = 0.8,) #Tamanho do texto
 
 #Capturando o valor da frequencia de cada faixa do histograma:
 frequencias <- histograma$counts
 colunas <- histograma$breaks
 
-#ignorar essas duas variaveis, eram utilizadas apenas no contexto de apenas
-#uma tabela para o histograma
-#faixas_inicio <- histograma$breaks[-length(histograma$breaks)]
-#faixas_fim <- histograma$breaks[-1]
 #-------------------------------------------------------------------------------
 #Definição das faixas e plotagem do histograma - Fim
 
@@ -122,3 +127,8 @@ if (escolhaBaseDeDados == "SQL") {
 #select <- "SELECT range_start, range_end, frequency FROM cpu_histogram
 #              WHERE dthCriacao > 2024-10-01 AND dthCriacao < 2024-10-31;"
 
+
+#ignorar essas duas variaveis, eram utilizadas apenas no contexto de apenas
+#uma tabela para o histograma
+#faixas_inicio <- histograma$breaks[-length(histograma$breaks)]
+#faixas_fim <- histograma$breaks[-1]
