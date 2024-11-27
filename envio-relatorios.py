76637aceb94b475ddfe3b04116926a19bdb02d3a
Marcio-Juliao <marcio.juliao@sptech.school>
2024-11-27 20:21:15 -0300

    Script Python de envio de relatórios

---
+++ envio-relatorios.py
@@ -0,0 +1,48 @@
+from dotenv import load_dotenv
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+import os
+
+load_dotenv()
+
+# Função para enviar arquivo ao Slack
+def enviar_para_slack(canal, caminho_arquivo, mensagem):
+    client = WebClient(token=os.getenv("TOKEN_SLACK"))
+
+    try:
+        response = client.files_upload_v2(
+            channels=canal,
+            initial_comment=mensagem,
+            file=caminho_arquivo
+        )
+        print("Arquivo enviado para o Slack com sucesso!")
+    except SlackApiError as e:
+        print(f"Erro ao enviar para o Slack: {e.response['error']}")
+
+# Função principal
+def main():
+
+    opcao = None
+    condicao = True
+    while condicao:
+        opcao = input("\033[1mDigite 1 para \033[31mRelatório Semanal\033[m ou 2 para \033[34mRelatório Mensal\033[m: ")
+        if opcao.isdigit():
+            opcao = int(opcao)
+            if opcao == 1 or opcao == 2:
+                condicao = False
+
+    caminho_relatorio = None
+    mensagem = None
+    if opcao == 1:
+        caminho_relatorio = "./relatorio_semanal.html"
+        mensagem = "Relatório Semanal de Monitoramento de Hardware: "
+    elif opcao == 2:
+        caminho_relatorio = "./relatorio_mensal.html"
+        mensagem = "Relatório Mensal de Monitoramento de Hardware: "
+
+    channel_id = "C081XHLS15M"
+
+    enviar_para_slack(channel_id, caminho_relatorio, mensagem)
+
+if __name__ == "__main__":
+    main()
